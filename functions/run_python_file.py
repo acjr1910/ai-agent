538a0d83a4458cbc371ca0ac459cb77dd9cf8970
acjr1910 <acjr1910@gmail.com>
2026-08-23 20:05:48 +0100

    feat: add write and run agent fn tools

---
+++ functions/run_python_file.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+
+
+def run_python_file(
+    working_directory: str, file_path: str, args: list[str] | None = None
+) -> str:
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        if os.path.commonpath([abs_working_dir, abs_file_path]) != abs_working_dir:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(abs_file_path):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        if not file_path.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file'
+
+        command = ["python", abs_file_path]
+        if args:
+            command.extend(args)
+        process = subprocess.run(
+            command, timeout=30, text=True, capture_output=True, cwd=abs_working_dir
+        )
+        output = ""
+
+        if process.returncode != 0:
+            output += f"Process exited with code {process.returncode}\n"
+        if not process.stderr and not process.stdout:
+            output += "No output produced\n"
+        if process.stdout:
+            output += f"STDOUT: {process.stdout}"
+        if process.stderr:
+            output += f"STDERR: {process.stderr}"
+        return output
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
